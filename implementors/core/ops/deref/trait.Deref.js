8d69927ccfda1768cb316f37f44a056c4c87493b
Inky-developer <Inky-developer@users.noreply.github.com>
2020-10-27 19:28:44 +0000

    deploy: 7d21cf22202ed3825ad92cc2a75518827fc3e6a9

--- implementors/core/ops/deref/trait.Deref.js
+++ implementors/core/ops/deref/trait.Deref.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
 implementors["debris_common"] = [{"text":"impl Deref for Span","synthetic":false,"types":[]}];
-implementors["debris_core"] = [{"text":"impl Deref for ObjectRef","synthetic":false,"types":[]},{"text":"impl Deref for ObjectString","synthetic":false,"types":[]}];
+implementors["debris_core"] = [{"text":"impl Deref for ObjectRef","synthetic":false,"types":[]},{"text":"impl Deref for ObjString","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -1,7 +1,7 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["debris_backends"] = {"name":"","dirs":[{"name":"common","files":["commands.rs","mod.rs"]},{"name":"datapack","files":["backend.rs","mod.rs","stringify.rs"]}],"files":["lib.rs"]};
 sourcesIndex["debris_common"] = {"name":"","files":["accessor.rs","ident.rs","input_file.rs","lib.rs","span.rs"]};
-sourcesIndex["debris_core"] = {"name":"","dirs":[{"name":"error","files":["mod.rs"]},{"name":"hir","files":["hir_impl.rs","hir_nodes.rs","identifier.rs","mod.rs"]},{"name":"llir","files":["llir_context.rs","llir_impl.rs","llir_nodes.rs","mod.rs","utils.rs"]},{"name":"mir","files":["mir_context.rs","mir_impl.rs","mir_nodes.rs","mod.rs","utils.rs"]},{"name":"objects","files":["mod.rs","obj_class.rs","obj_function.rs","obj_integer_dyn.rs","obj_integer_static.rs","obj_module.rs","obj_string.rs"]}],"files":["compile_context.rs","config.rs","database.rs","debris_object.rs","inputs.rs","lib.rs","types.rs"]};
+sourcesIndex["debris_core"] = {"name":"","dirs":[{"name":"error","files":["mod.rs"]},{"name":"hir","files":["hir_impl.rs","hir_nodes.rs","identifier.rs","mod.rs"]},{"name":"llir","files":["llir_context.rs","llir_impl.rs","llir_nodes.rs","mod.rs","utils.rs"]},{"name":"mir","files":["mir_context.rs","mir_impl.rs","mir_nodes.rs","mod.rs","utils.rs"]},{"name":"objects","files":["mod.rs","obj_class.rs","obj_function.rs","obj_integer.rs","obj_integer_static.rs","obj_module.rs","obj_string.rs"]}],"files":["compile_context.rs","config.rs","database.rs","debris_object.rs","inputs.rs","lib.rs","types.rs"]};
 sourcesIndex["debris_derive"] = {"name":"","files":["lib.rs","object.rs","utils.rs"]};
 sourcesIndex["debris_lang"] = {"name":"","files":["main.rs"]};
 sourcesIndex["debris_std"] = {"name":"","files":["lib.rs"]};
