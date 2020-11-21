9211937bb112cd9fca68a39610be3b6b566e4b17
Inky-developer <Inky-developer@users.noreply.github.com>
2020-11-21 13:56:54 +0000

    deploy: 560e4f040847615b5a48598644a0f32fae74d1ab

--- implementors/pest/parser/trait.Parser.js
+++ implementors/pest/parser/trait.Parser.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["debris_core"] = [{"text":"impl Parser&lt;Rule&gt; for ArithmeticParser","synthetic":false,"types":[]}];
+implementors["debris_core"] = [{"text":"impl Parser&lt;Rule&gt; for DebrisParser","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
