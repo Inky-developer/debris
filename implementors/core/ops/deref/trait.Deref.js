d81f4459b5ffa9f7f028aafa00b3aa07559a92ea
Inky-developer <Inky-developer@users.noreply.github.com>
2020-12-25 17:22:42 +0000

    deploy: 50e6b2c37b4255a3ee84d82c921349939321cf3c

--- implementors/core/ops/deref/trait.Deref.js
+++ implementors/core/ops/deref/trait.Deref.js
@@ -1,4 +1,3 @@
 (function() {var implementors = {};
-implementors["debris_common"] = [{"text":"impl Deref for Span","synthetic":false,"types":[]}];
 implementors["debris_core"] = [{"text":"impl Deref for ObjectRef","synthetic":false,"types":[]},{"text":"impl Deref for ObjString","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
