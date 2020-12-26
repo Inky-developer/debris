9e3a9242981b75a7ca049febe4c3c8995cd5c486
Inky-developer <Inky-developer@users.noreply.github.com>
2020-12-26 14:06:05 +0000

    deploy: 76e1132aebbb3d1b275a9ee7d38135d957b45e09

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["debris_core"] = [{"text":"impl Error for CompileError","synthetic":false,"types":[]},{"text":"impl Error for ParseError","synthetic":false,"types":[]},{"text":"impl Error for LangError","synthetic":false,"types":[]},{"text":"impl Error for LangErrorKind","synthetic":false,"types":[]}];
+implementors["debris_core"] = [{"text":"impl Error for ParseError","synthetic":false,"types":[]},{"text":"impl Error for LangError","synthetic":false,"types":[]},{"text":"impl Error for LangErrorKind","synthetic":false,"types":[]},{"text":"impl Error for CompileError","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
