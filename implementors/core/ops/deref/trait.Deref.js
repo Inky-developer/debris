a40cd55f68220a19f594641adf035e430ee159a1
Inky-developer <Inky-developer@users.noreply.github.com>
2020-12-30 19:33:04 +0000

    deploy: 0d0dd4893dcfb4098486dddf2e79c01279f5ded1

--- implementors/core/ops/deref/trait.Deref.js
+++ implementors/core/ops/deref/trait.Deref.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["debris_core"] = [{"text":"impl Deref for ObjectRef","synthetic":false,"types":[]},{"text":"impl Deref for ObjString","synthetic":false,"types":[]}];
+implementors["debris_core"] = [{"text":"impl Deref for NamespaceArena","synthetic":false,"types":[]},{"text":"impl Deref for ObjectRef","synthetic":false,"types":[]},{"text":"impl Deref for ObjString","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
