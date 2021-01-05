69be5ff6cb84822ec906bfabd855d5412d2994d6
Inky-developer <Inky-developer@users.noreply.github.com>
2021-01-05 14:01:08 +0000

    deploy: 63af1f81e9adffa1785718007db754df466d3452

--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
 implementors["debris_common"] = [{"text":"impl Default for InputFiles","synthetic":false,"types":[]}];
-implementors["debris_core"] = [{"text":"impl Default for NamespaceArena","synthetic":false,"types":[]},{"text":"impl&lt;'ctx&gt; Default for Mir&lt;'ctx&gt;","synthetic":false,"types":[]},{"text":"impl Default for CompileContext","synthetic":false,"types":[]},{"text":"impl Default for Config","synthetic":false,"types":[]}];
+implementors["debris_core"] = [{"text":"impl Default for NamespaceArena","synthetic":false,"types":[]},{"text":"impl&lt;'ctx&gt; Default for Mir&lt;'ctx&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'ctx&gt; Default for MirContextMap&lt;'ctx&gt;","synthetic":false,"types":[]},{"text":"impl Default for CompileContext","synthetic":false,"types":[]},{"text":"impl Default for Config","synthetic":false,"types":[]}];
 implementors["vfs"] = [{"text":"impl Default for File","synthetic":false,"types":[]},{"text":"impl Default for Directory","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
