bdb35b82481b9bfc7a02b8675d6e44e524813c4e
Inky-developer <Inky-developer@users.noreply.github.com>
2021-04-05 20:30:39 +0000

    deploy: 4fef2bfcaffb529f423ca7b667b55ac2fff50bfe

---
+++ debris_core/objects/obj_format_string/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["FormatStringComponent",""]],"struct":[["ObjFormatString","A static string object"]]});
--- debris_core/objects/sidebar-items.js
+++ debris_core/objects/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["obj_bool",""],["obj_bool_static",""],["obj_class",""],["obj_function",""],["obj_int",""],["obj_int_static",""],["obj_module",""],["obj_native_function",""],["obj_never",""],["obj_null",""],["obj_string",""]]});
+initSidebarItems({"mod":[["obj_bool",""],["obj_bool_static",""],["obj_class",""],["obj_format_string",""],["obj_function",""],["obj_int",""],["obj_int_static",""],["obj_module",""],["obj_native_function",""],["obj_never",""],["obj_null",""],["obj_string",""]]});
