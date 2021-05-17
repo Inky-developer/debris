f52b7ff428bc107b501f2a34fa610a344833f482
Inky-developer <Inky-developer@users.noreply.github.com>
2021-05-17 17:46:50 +0000

    deploy: 16bed8eda2ed9b24c9cf6b1b227e65d61aecf417

--- debris_core/objects/obj_bool/sidebar-items.js
+++ debris_core/objects/obj_bool/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["and_static",""],["or_static","Returns the boolean or-ed with the static value"]],"struct":[["ObjBool","A boolean value that is stored on a scoreboard"]]});
+initSidebarItems({"fn":[["and_static",""],["cmp",""],["or_static","Returns the boolean or-ed with the static value"]],"struct":[["ObjBool","A boolean value that is stored on a scoreboard"]]});
