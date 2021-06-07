8c06a008be89b8cc123952684a43242b54d6a802
Inky-developer <developerinky@gmail.com>
2021-06-07 14:20:02 +0200

    Extract redundant_copy_optimizer

--- debris/core/src/llir/opt/optimizers/mod.rs
+++ debris/core/src/llir/opt/optimizers/mod.rs
@@ -6,6 +6,7 @@ mod callchain_optimizer;
 mod common_path_optimizer;
 mod const_optimizer;
 mod redundancy_optimizer;
+mod redundant_copy_optimizer;
 
 pub use alias_function_optimizer::optimize_alias_function;
 pub use arithmetic_optimizer::simple_arithmetic_optimization;
@@ -13,3 +14,4 @@ pub use callchain_optimizer::optimize_call_chain;
 pub use common_path_optimizer::optimize_common_path;
 pub use const_optimizer::ConstOptimizer;
 pub use redundancy_optimizer::RedundancyOptimizer;
+pub use redundant_copy_optimizer::RedundantCopyOptimizer;
