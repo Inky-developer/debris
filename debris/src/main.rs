abe03ce84f1f44cf426d3619caefa5ee6f727efa
Inky-developer <developerinky@gmail.com>
2021-12-20 11:16:30 +0100

    Clean up comment in main.rs

--- debris/src/main.rs
+++ debris/src/main.rs
@@ -3,12 +3,10 @@
 //! Currently, the compiler is in a very early state.
 //! Look at [`debug_run`] to see how to compile a script.
 //!
-//! Most of the logic of the compiler is in [debris_core].
+//! [`debris_std`] contains the standard library, which is implicitly imported into every script.
 //!
-//! [debris_std] contains the standard library, which is implicitly imported into every script.
-//!
-//! The compiler is mostly backend agnostic and only generates a [low-level intermediate representation](debris_core::llir).
-//! The crate [debris_backends] crate contains backend implementations that can convert this ir into files.
+//! The compiler is mostly backend agnostic and only generates a [low-level intermediate representation](debris_llir).
+//! The crate [`debris_backends`] crate contains backend implementations that can convert this ir into files.
 //! Right now, the only backend implementation that exists converts the llir code into datapacks.
 //! Backends that can create command blocks or even executables might be added in the future.
 
@@ -75,7 +73,7 @@ fn main() {
 fn init() -> CompileConfig {
     let mut compile_config = CompileConfig::new("examples".into());
     let mut args = env::args();
-    let build_mode = args.nth(1).map_or(Default::default(), |arg| {
+    let build_mode = args.nth(1).map_or(BuildMode::default(), |arg| {
         if arg.eq("release") {
             BuildMode::Release
         } else {
